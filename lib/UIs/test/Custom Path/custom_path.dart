20a43d60a8b18bb02c80c77dccb30b5cbe8c608e
Arab Kumar <arab@fello.in>
2022-04-28 16:51:23 +0530

    added custom path with svg background

--- lib/UIs/test/Custom Path/custom_path.dart
+++ lib/UIs/test/Custom Path/custom_path.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:testapp/utils/size_config.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomPath extends StatefulWidget {
   const CustomPath({Key? key}) : super(key: key);
@@ -20,7 +21,7 @@ class CustomPathState extends State<CustomPath>
   @override
   void initState() {
     _controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 5000));
+        vsync: this, duration: const Duration(milliseconds: 15000));
     super.initState();
     _animation = Tween(begin: 0.0, end: 1.0).animate(_controller!)
       ..addListener(() {
@@ -34,6 +35,8 @@ class CustomPathState extends State<CustomPath>
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = SizeConfig.height! * 4;
+    log("X: ${calculateX(_animation!.value).dx.toString()} Y: ${calculateY(_animation!.value).dy.toString()}");
     return Scaffold(
       backgroundColor: Colors.black,
       body: SizedBox(
@@ -44,40 +47,74 @@ class CustomPathState extends State<CustomPath>
           child: Container(
             height: SizeConfig.height! * 4,
             width: SizeConfig.width,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://img.freepik.com/free-photo/landscape-tropical-green-forest_181624-30814.jpg?t=st=1650979519~exp=1650980119~hmac=84e85546784bcfada1418551efbcba25b6568eb29df0729307bcbdfa637b6097&w=1800"),
-                fit: BoxFit.fitHeight,
-              ),
-            ),
+            color: Colors.black,
+            // decoration: const BoxDecoration(
+            //   image: DecorationImage(
+            //     image: NetworkImage(
+            //         "https://img.freepik.com/free-photo/landscape-tropical-green-forest_181624-30814.jpg?t=st=1650979519~exp=1650980119~hmac=84e85546784bcfada1418551efbcba25b6568eb29df0729307bcbdfa637b6097&w=1800"),
+            //     fit: BoxFit.fitHeight,
+            //   ),
+            // ),
             child: Stack(
               children: [
+                SizedBox(
+                  height: SizeConfig.height! * 4,
+                  width: SizeConfig.width,
+                  child: ListView.builder(
+                      reverse: true,
+                      padding: EdgeInsets.zero,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 5,
+                      itemBuilder: (ctx, i) {
+                        return SvgPicture.asset(
+                          i % 2 == 0
+                              ? "assets/custompathassets/bg1.svg"
+                              : "assets/custompathassets/bg2.svg",
+                          width: SizeConfig.width,
+                        );
+                      }),
+                ),
                 Positioned(
                   bottom: 0,
                   left: 0,
                   child: CustomPaint(
-                    size: Size(SizeConfig.width!, SizeConfig.height!),
+                    size: Size(SizeConfig.width!, SizeConfig.height! * 4),
                     painter: PathPainter(_path!),
                   ),
                 ),
                 Positioned(
-                    top: SizeConfig.height! * 3 +
-                        calculateY(_animation!.value).dy,
-                    left: calculateX(_animation!.value).dx,
-                    child: const CircleAvatar(
-                      radius: 15,
-                      backgroundImage: NetworkImage(
-                          "https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1651060697~hmac=cda480a2e7fb04d234469475db03456b"),
-                    )
-                    // Container(
-                    //   decoration: BoxDecoration(
-                    //       color: Colors.red,
-                    //       borderRadius: BorderRadius.circular(10)),
-                    //   width: 20,
-                    //   height: 20,
-                    // ),
-                    ),
+                  top:
+                      SizeConfig.height! * 3 + calculateY(_animation!.value).dy,
+                  left: calculateX(_animation!.value).dx,
+                  child: const CircleAvatar(
+                    radius: 15,
+                    backgroundImage: NetworkImage(
+                        "https://firebasestorage.googleapis.com/v0/b/fello-dev-station.appspot.com/o/test%2Favatar.png?alt=media&token=bab2c849-7694-41c4-9c34-1ab022799bfd"),
+                  ),
+                ),
+                Positioned(
+                  child: GestureDetector(
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            duration: Duration(seconds: 2),
+                            content: Text("You Tapped a Tree"),
+                          ),
+                        );
+                      },
+                      child:
+                          SvgPicture.asset("assets/custompathassets/tree.svg")),
+                  bottom: screenHeight * 0.02,
+                  left: SizeConfig.width! * 0.05,
+                )
+                // Container(
+                //   decoration: BoxDecoration(
+                //       color: Colors.red,
+                //       borderRadius: BorderRadius.circular(10)),
+                //   width: 20,
+                //   height: 20,
+
+                //),
               ],
             ),
           ),
@@ -93,28 +130,37 @@ class CustomPathState extends State<CustomPath>
   }
 
   Path drawPath() {
-    double? screenWidth = SizeConfig.width;
-    double? screenHeight = SizeConfig.height!;
-    Size size = Size(screenWidth!, screenHeight);
+    double screenWidth = SizeConfig.width!;
+    double screenHeight = SizeConfig.height! * 4;
+    // Size size = Size(screenWidth!, screenHeight);
     Path path = Path()
-      ..moveTo(size.width / 2, size.height * 0.9)
-      ..cubicTo(0, size.height * 0.8, 0, size.height * 0.65, size.width * 0.6,
-          size.height * 0.57)
-      ..cubicTo(size.width, size.height * 0.5, size.width, size.height * 0.3,
-          size.width * 0.2, size.height * 0.3);
+      ..moveTo(screenWidth * 0.6, screenHeight)
+      ..cubicTo(screenWidth * 0.9, screenHeight * 0.98, screenWidth,
+          screenHeight * 0.955, screenWidth * 0.5, screenHeight * 0.95)
+      ..cubicTo(screenWidth * 0.15, screenHeight * 0.95, 0, screenHeight * 0.93,
+          screenWidth * 0.5, screenHeight * 0.92)
+      ..cubicTo(screenWidth, screenHeight * 0.91, screenWidth,
+          screenHeight * 0.9, screenWidth * 0.85, screenHeight * 0.885)
+      ..quadraticBezierTo(screenWidth * 0.85, screenHeight * 0.86,
+          screenWidth * 0.7, screenHeight * 0.863)
+      ..lineTo(screenWidth * 0.6, screenHeight * 0.855)
+      ..cubicTo(screenWidth * 0.15, screenHeight * 0.85, 0, screenHeight * 0.82,
+          screenWidth * 0.5, screenHeight * 0.815)
+      ..cubicTo(screenWidth, screenHeight * 0.81, screenWidth,
+          screenHeight * 0.77, screenWidth * 0.5, screenHeight * 0.77);
 
-    // ..quadraticBezierTo(size.width * 1.5, size.height * 0.8, size.width * 0.5,
-    //     size.height * 0.9)
+    // ..quadraticBezierTo(screenWidth * 1.5, screenHeight * 0.8, screenWidth * 0.5,
+    //     screenHeight * 0.9)
 
-    // path.lineTo(size.width / 2, size.height * 0.8);
-    // path.moveTo(size.width * 0.2, size.height * 0.9);
+    // path.lineTo(screenWidth / 2, screenHeight * 0.8);
+    // path.moveTo(screenWidth * 0.2, screenHeight * 0.9);
 
     // path.quadraticBezierTo(
-    //     -1.5, size.height * 0.6, size.width * 0.8, size.height * 0.6);
-    // path.quadraticBezierTo(size.width * 1.5, size.height * 0.4,
-    //     size.width * 0.2, size.height * 0.5);
+    //     -1.5, screenHeight * 0.6, screenWidth * 0.8, screenHeight * 0.6);
+    // path.quadraticBezierTo(screenWidth * 1.5, screenHeight * 0.4,
+    //     screenWidth * 0.2, screenHeight * 0.5);
     // path.quadraticBezierTo(
-    //     -1.5, size.height * 0.2, size.width * 0.8, size.height * 0.4);
+    //     -1.5, screenHeight * 0.2, screenWidth * 0.8, screenHeight * 0.4);
     return path;
   }
 
@@ -145,9 +191,10 @@ class PathPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
-      ..color = Colors.amber
+      ..color = Colors.red
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 50.0;
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 30.0;
 
     canvas.drawPath(this.path, paint);
   }
